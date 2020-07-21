cec992eed61cd0d7077c36d06caa2c61b46ff411
lizhenting96 <lizhenting961113@gmail.com>
2020-07-21 10:41:52 -0700

    Empty script page

--- my-react-app/src/Components/DashboardSelect.js
+++ my-react-app/src/Components/DashboardSelect.js
@@ -11,11 +11,9 @@ import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { mainListItems } from "./listItems";
-import SelectForm from "./SelectForm"
 // import InteractiveTable from './MUIDatatable'
 import MainTable from './MainTable';
 
@@ -109,7 +107,6 @@ export default function Dashboard() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
     <div className={classes.root}>
